f5050cd880ce41e9403598abe81cbf120fa721b9
Hsiang-Jen Li <hsiangjenli@gmail.com>
2025-03-14 11:20:31 +0800

    feat: enhance preprocessing script by dropping unnecessary columns, adding data type checks, and visualizing distributions

--- src/dcc/preprocessing/test.py
+++ src/dcc/preprocessing/test.py
@@ -1,25 +1,45 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+import seaborn as sns
 
 df = pd.read_csv("data/raw/UCI_Credit_Card.csv")
-df = df.drop(columns=["default.payment.next.month"])  # Drop target column
+df = df.drop(columns=["ID", "default.payment.next.month"])  # Drop target column
 
 # Category columns
 pay_status_cols = [
     col for col in df.columns if col.startswith("PAY_") and not col.startswith("PAY_AMT")
 ]
 cat_cols = ["SEX", "EDUCATION", "MARRIAGE"]
-
 cat_cols.extend(pay_status_cols)
+
+print(f"Category columns: {cat_cols}")
 
 # Numerical columns
 num_cols = [col for col in df.columns if col not in cat_cols]
+
+# Double check if all columns' types are correct
+df[num_cols] = df[num_cols].astype("float64")
+df[cat_cols] = df[cat_cols].astype("category")
+
+# Describe numerical columns
+num_describe = df[num_cols].describe()
+num_describe.to_csv("outputs/numerical_describe.csv")
+
+print(f"Numerical columns: {num_cols}")
 
 # Visualize categorical columns
 for col in cat_cols:
     df[col].value_counts().sort_index().to_frame().plot(kind="bar")
     plt.savefig(f"outputs/images/{col}_countplot.png")
+
+# Visualize numerical columns
+for col in num_cols:  # 選三個關鍵變數
+    plt.figure(figsize=(12, 6))
+    sns.histplot(df[col], bins=50, kde=True)
+    plt.title(col)
+    plt.savefig(f"outputs/images/{col}_histplot.png")
+    plt.close()
 
 # Standardize numerical columns
 ss = StandardScaler()
@@ -27,7 +47,7 @@ df[num_cols] = ss.fit_transform(df[num_cols])
 
 # Transform categorical into dummies columns
 # For later use in the model
-df_cat_dummies = pd.get_dummies(df, columns=cat_cols)
+df_cat_dummies = pd.get_dummies(df, columns=cat_cols, drop_first=True)
 
 # Combine numerical and dummies columns
 df = pd.concat([df[num_cols], df_cat_dummies], axis=1)
