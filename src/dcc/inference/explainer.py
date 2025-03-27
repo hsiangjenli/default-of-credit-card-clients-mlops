c90e78e9f6cb2e146560564b33056f138ac44528
Hsiang-Jen Li <hsiangjenli@gmail.com>
2025-03-27 17:26:15 +0800

    feat: integrate eli5 for model explanation and update visualizations in explainer script

--- src/dcc/inference/explainer.py
+++ src/dcc/inference/explainer.py
@@ -2,6 +2,7 @@ import shap
 import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
+import eli5
 
 x_train = pd.read_csv("data/processed/train_x.csv")
 x_test = pd.read_csv("data/processed/test_x.csv")
@@ -18,17 +19,26 @@ x_col, model = load_data("train")
 explainer = shap.Explainer(model, x_train[x_col], feature_names=x_col)
 shap_values = explainer(x_test[x_col])
 
-shap.plots.beeswarm(shap_values)
+pred = eli5.explain_weights(model, feature_names=list(x_col))
+print(eli5.format_as_dataframe(pred))
+
+pred = eli5.explain_prediction_sklearn(model, x_test[x_col].iloc[0], feature_names=list(x_col))
+print(eli5.format_as_dataframe(pred))
+
+shap.plots.beeswarm(shap_values, max_display=100)
 plt.savefig("data/processed/shap_train.png", bbox_inches="tight")
 plt.close()
 
-# Train 2 explainer
-x_col, model = load_data("train-delay-only")
-explainer = shap.Explainer(model, x_train[x_col], feature_names=x_col)
-shap_values = explainer(x_test[x_col])
+# # Train 2 explainer
+# x_col, model = load_data("train-delay-only")
+# explainer = shap.Explainer(model, x_train[x_col], feature_names=x_col)
+# shap_values = explainer(x_test[x_col])
 
-shap.plots.beeswarm(shap_values)
-plt.savefig("data/processed/shap_train-delay-only.png", bbox_inches="tight")
+# pred = eli5.explain_weights(model, feature_names=list(x_col))
+# print(eli5.format_as_dataframe(pred))
+
+# shap.plots.beeswarm(shap_values, max_display=100)
+# plt.savefig("data/processed/shap_train-delay-only.png", bbox_inches="tight")
 
 # Train 3 explainer
 # x_col, model = load_data("train-delay-num")
